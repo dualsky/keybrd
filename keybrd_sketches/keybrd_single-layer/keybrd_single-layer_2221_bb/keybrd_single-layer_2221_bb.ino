2c8a8baa36f5f6bbc3a64046ee86fd9c771c01fd
wolfv6 <wolfv6@users.noreply.github.com>
2015-04-19 16:46:07 -0600

    2221 matrix_R tested

--- keybrd_sketches/keybrd_single-layer/keybrd_single-layer_2221_bb/keybrd_single-layer_2221_bb.ino
+++ keybrd_sketches/keybrd_single-layer/keybrd_single-layer_2221_bb/keybrd_single-layer_2221_bb.ino
@@ -2,7 +2,7 @@
         1       2       3       4
         a       b       c       d
 */
-// ********** INCLUDES **********
+// ========== INCLUDES ==========
 //Arduino library files
 #include <Keyboard.h>
 #include <Wire.h>
@@ -11,17 +11,20 @@
 #include <objects_key.h>
 #include <c_Row_Ex.h>
 #include <c_RowWait.h>
-#include <c_Matrix_Exp.h>?
-#include <c_PortRows.h>
-#include <c_PortsRows.h>
-#include <c_PortCols.h>
-#include <c_PortsCols.h>
+#include <c_RowPort_AVR.h>
+#include <c_ColPort_AVR.h>
+#include <c_RowPort_MCP23018.h>
+#include <c_ColPort_MCP23018.h>
 #include <c_Matrix.h>
 
+//keybrd_Layers library files
 #include <c_Keybrd.h>
 #include <l_LayerManager.h>
 
-// *********** LEFT ROWS OF KEYS ************
+// =============== LEFT =====================
+
+/*
+// ----------- LEFT ROWS OF KEYS ------------
 //row0
 c_Key* const ptrsKey_L0[] = {     &k_1,           &k_2         };
 c_Row_Ex row_L0(ptrsKey_L0, 2);
@@ -30,7 +33,16 @@ c_Row_Ex row_L0(ptrsKey_L0, 2);
 c_Key* const ptrsKey_L1[] = {     &k_a,           &k_b         };
 c_Row_Ex row_L1(ptrsKey_L1, 2);
 
-// *********** RIGHT ROWS OF KEYS ************
+// ------------- LEFT MATRIX --------------
+c_Row_Ex* const ptrsRow_L[] = { &row_L0, &row_L1 };
+const uint8_t ROWS_L_COUNT = sizeof(ptrsRow_L)/sizeof(ptrsRow_L[0]);
+
+//the c_Matrix_Exp arguements are for the MCP23018 I/O Expander
+c_Matrix matrix_L(ptrsRow_L, ROWS_L_COUNT, 0x20, 0x12, 0x0C, 0x13, 0x01);
+
+*/
+// =============== RIGHT ====================
+// ----------- RIGHT ROWS OF KEYS ------------
 //row0
 c_Key* const ptrsKey_R0[] = {     &k_3,           &k_4         };
 c_Row_Ex row_R0(ptrsKey_R0, 2);
@@ -40,44 +52,34 @@ c_Key* const ptrsKey_R1[] = {     &k_c,           &k_d         };
 c_Row_Ex row_R1(ptrsKey_R1, 2);
 
 //static variables
-c_Row_Ex* const ptrsRow[] = { &row_R0, &row_R1 };
-c_Row_Ex* const* const c_PortRows::ptrsRows = ptrsRow;
+c_Row_Ex* const ptrsRows[] = { &row_R0, &row_R1 };
+c_Row_Ex* const* const c_RowPort::ptrsRows = ::ptrsRows;
 
 c_RowWait rowWait(4, 10);
 c_RowWait& c_Row_Ex::refRowWait = rowWait;
 
-/*************** ROW PORTS *************
- * row: 0   1
- * pin: F0  F1
- */
-c_PortRows rowPort_F(DDRF, PORTF, 1<<0 | 1<<1 );
-c_PortRows* rowsPorts[] = { &rowPort_F };
-c_PortsRows rows(rowsPorts, 1);
+// --------------- RIGHT ROW PORTS -------------
+// row: 0   1
+// pin: F0  F1
 
-/*************** COL PORTS *************
- * col: 0   1
- * pin: B0  B1
- */
-c_PortCols colPort_B(DDRB, PORTB, PINB, 1<<0 | 1<<1 );
-c_PortCols* colPorts[] = { &colPort_B };
-c_PortsCols cols(colPorts, 1);
+c_RowPort_AVR rowPortF(DDRF, PORTF, 1<<0 | 1<<1 );
+c_RowPort* ptrsRowPorts_R[] = { &rowPortF };
 
-// ************* MATRICES **************
-//matrix_L
-c_Row_Ex* const ptrsRow_L[] = { &row_L0, &row_L1 };
-const uint8_t ROWS_L_COUNT = sizeof(ptrsRow_L)/sizeof(ptrsRow_L[0]);
+// --------------- RIGHT COL PORTS -------------
+// col: 0   1
+// pin: B0  B1
 
-//the c_Matrix_Exp arguements are for the MCP23018 I/O Expander
-c_Matrix matrix_L(ptrsRow_L, ROWS_L_COUNT, 0x20, 0x12, 0x0C, 0x13, 0x01);
+c_ColPort_AVR colPortB(DDRB, PORTB, PINB, 1<<0 | 1<<1 );
+c_ColPort* ptrsColPorts_R[] = { &colPortB };
 
-//matrix_R
-c_Matrix matrix_R(&rows, &cols);
+// ------------- RIGHT MATRIX --------------
+c_Matrix matrix_R(ptrsRowPorts_R, 1, ptrsColPorts_R, 1);
 
-// ************** KEYBOARD *************
+// ============== KEYBOARD =============
 //c_Matrix* const ptrsMatrix[] = { &matrix_L, &matrix_R };
 c_Matrix* const ptrsMatrix[] = { &matrix_R };
 c_Keybrd keybrd(ptrsMatrix, 1);
 
-// **************** RUN ****************
+// ================ RUN ================
 void setup() { keybrd.begin(); }
 void loop()  { keybrd.scan();  }
